7b1242c663ae06154e4a3c26dd5d9e79b4634239
Lmehdi Ouabi <lmehdi@autosense.ch>
2021-01-25 18:46:48 +0100

    Fix issue

--- lib/src/firebase_connector.dart
+++ lib/src/firebase_connector.dart
@@ -1,23 +1,39 @@
+import 'package:flutter_apns/apns.dart';
 import 'package:flutter_apns/src/connector.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 
 class FirebasePushConnector extends PushConnector {
   final firebaseMessaging = FirebaseMessaging.instance;
-  final firebasePushConnector = FirebasePushConnector();
+  // final firebasePushConnector = FirebasePushConnector();
 
   @override
   final isDisabledByUser = ValueNotifier(false);
 
   @override
   void configure({onMessage, onLaunch, onResume, onBackgroundMessage}) {
-
-    firebasePushConnector.configure(
+    /*firebaseMessaging.configure(
       onMessage: onMessage,
       onLaunch: onLaunch,
       onResume: onResume,
       onBackgroundMessage: onBackgroundMessage,
-    );
+    );*/
+
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage message) {
+      onLaunch(message.data);
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      onMessage(message.data);
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      onResume(message.data);
+    });
+
+    FirebaseMessaging.onBackgroundMessage((message) => onBackgroundMessage(message.data));
 
     firebaseMessaging.onTokenRefresh.listen((value) {
       token.value = value;
